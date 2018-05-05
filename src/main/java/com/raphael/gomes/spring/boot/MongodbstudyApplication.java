9b833083c27780579c5f66e357b6b786a222718d
Raphael F. Gomes <raphaelg9@gmail.com>
2018-05-05 00:24:48 -0300

    Creating the project

---
+++ src/main/java/com/raphael/gomes/spring/boot/MongodbstudyApplication.java
@@ -0,0 +1,12 @@
+package com.raphael.gomes.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbstudyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/raphael/gomes/spring/boot/MongodbstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raphael.gomes.spring.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongodbstudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
