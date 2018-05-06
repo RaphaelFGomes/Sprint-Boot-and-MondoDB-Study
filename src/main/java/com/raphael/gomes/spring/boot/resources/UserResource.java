005632253cd18170f3e4a78bab29d7b44a95dd1e
Raphael F. Gomes <raphaelg9@gmail.com>
2018-05-06 12:54:48 -0300

    Adding CRUD - Feedback

--- src/main/java/com/raphael/gomes/spring/boot/resources/UserResource.java
+++ src/main/java/com/raphael/gomes/spring/boot/resources/UserResource.java
@@ -1,16 +1,17 @@
 package com.raphael.gomes.spring.boot.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.raphael.gomes.spring.boot.domain.User;
 import com.raphael.gomes.spring.boot.dto.UserDTO;
@@ -35,5 +36,29 @@ public class UserResource {
 		User obj = service.findById(id);		
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		// Code 201
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		// Code 204
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		// Code 204
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/raphael/gomes/spring/boot/services/UserService.java
+++ src/main/java/com/raphael/gomes/spring/boot/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raphael.gomes.spring.boot.domain.User;
+import com.raphael.gomes.spring.boot.dto.UserDTO;
 import com.raphael.gomes.spring.boot.repository.UserRepository;
 import com.raphael.gomes.spring.boot.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,29 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete (String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update (User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData (User newUser, User oldUser) {
+		newUser.setName(oldUser.getName());
+		newUser.setEmail(oldUser.getEmail());
+	}
+	
+	public User fromDTO (UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());		
 	}
 }
