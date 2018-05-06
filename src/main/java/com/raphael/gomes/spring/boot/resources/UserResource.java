de0a2c6c8b286b34bc5443e32d0ccc30029e1877
Raphael F. Gomes <raphaelg9@gmail.com>
2018-05-06 10:48:37 -0300

    Adding Standard exception

--- src/main/java/com/raphael/gomes/spring/boot/resources/UserResource.java
+++ src/main/java/com/raphael/gomes/spring/boot/resources/UserResource.java
@@ -3,8 +3,11 @@ package com.raphael.gomes.spring.boot.resources;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,12 @@ public class UserResource {
 		List<User> listUser = service.findAll();	
 		List<UserDTO> listUserDTO = listUser.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok(listUserDTO);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);		
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 
 }
